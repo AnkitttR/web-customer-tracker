34ad89d99bac8fdb1c075ce634a2b9b2957d1260
Ankit <ankitthakur1998@gmail.com>
2022-04-25 19:32:54 +0530

    Try Catch block added

--- src/com/luv2code/testdb/TestDbServlet.java
+++ src/com/luv2code/testdb/TestDbServlet.java
@@ -25,8 +25,16 @@ public class TestDbServlet extends HttpServlet {
 		String pass = "springstudent";
 		
 		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+		String driver = "com.mysql.cj.jdbc.Driver";
 		
 		//get connection to database
+		try {
+			
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+			throw new ServletException(exc);
+		}
 		
 		
 	}

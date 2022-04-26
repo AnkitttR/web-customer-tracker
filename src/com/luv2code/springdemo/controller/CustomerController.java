6e692be1bc093312821545d8b91905841bc8f6ab
Ankit <ankitthakur1998@gmail.com>
2022-04-26 12:52:52 +0530

    Codes added

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
@@ -19,10 +22,11 @@ public class CustomerController {
 	public String listCustomers(Model theModel) {
 		
 		//get customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers(); //Hey customerDAO give me Customers
 		
 		//add the customers to the model
-		
-		
+		theModel.addAttribute("customers", theCustomers);  //Hey theModel I will add Attribute intoyou.
+		                      //"name"  , value
 		
 		return "list-customers";   //list-customers.jsp page
 	}

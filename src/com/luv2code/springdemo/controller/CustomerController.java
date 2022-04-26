71177b8ca521004d7a70e127fb79a9864795e5fb
Ankit <ankitthakur1998@gmail.com>
2022-04-26 18:06:14 +0530

    @GetMapping is used in CustomerController

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
@@ -18,7 +19,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")   //Jab hum /customer/list pe honge toh, list-customers.jsp page open hoga.
+	@GetMapping("/list")   //Jab hum /customer/list pe honge toh, list-customers.jsp page open hoga.
 	public String listCustomers(Model theModel) {
 		
 		//get customers from the dao

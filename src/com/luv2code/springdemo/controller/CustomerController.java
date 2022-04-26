b6ec0155f8c13451ac1d025741ef3a7dbbe0b8f1
Ankit <ankitthakur1998@gmail.com>
2022-04-26 12:42:17 +0530

    CustomerDAO is injected in CustomerController

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,13 +1,20 @@
 package com.luv2code.springdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	//need to inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")   //Jab hum /customer/list pe honge toh, list-customers.jsp page open hoga.
 	public String listCustomers(Model theModel) {
 		
--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -6,10 +6,13 @@ import javax.transaction.Transactional;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.luv2code.springdemo.entity.Customer;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
 
 	// need to inject the session factory
@@ -25,12 +28,14 @@ public class CustomerDAOImpl implements CustomerDAO {
 		Session currentSession = sessionFactory.getCurrentSession();
 		
 		//create a query
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
 		
 		//execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
 		
 		//return the results
 		
-		return null;
+		return customers;
 	}
 
 }

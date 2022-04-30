58972efa6a10010527218e95b349064e595ae5d4
Ankit <ankitthakur1998@gmail.com>
2022-04-30 10:36:41 +0530

    Logger is setted up

--- src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.aspect;
 
+import java.util.logging.Logger;
+
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class CRMLoggingAspect {
 
 	// setup logger
-	
+	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// setup pointcut declarations
 	
